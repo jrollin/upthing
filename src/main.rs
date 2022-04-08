fbd8a38e2526531df8a3d4bc507f1123ea04adca
jrollin <1086565+jrollin@users.noreply.github.com>
2022-04-09 00:10:00 +0200

    feat: add health check

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer};
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
+        .bind("127.0.0.1:8000")?
+        .run()
+        .await
+}
+
+async fn health_check(_req: HttpRequest) -> HttpResponse {
+    HttpResponse::Ok().into()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use actix_web::{
+        http::{self, header::ContentType},
+        test,
+    };
+
+    #[actix_web::test]
+    async fn test_health_ok() {
+        let req = test::TestRequest::default()
+            .insert_header(ContentType::plaintext())
+            .to_http_request();
+        let resp = health_check(req).await;
+        assert_eq!(resp.status(), http::StatusCode::OK);
+    }
 }
